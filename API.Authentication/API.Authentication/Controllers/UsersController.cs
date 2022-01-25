fa9fd3da979a3b88e9d6ec05a2d86e46bb6c906c
Joao Gouveia <jevg@live.com.pt>
2022-01-25 23:51:37 +0000

    Add automapper

--- API.Authentication/API.Authentication/Controllers/UsersController.cs
+++ API.Authentication/API.Authentication/Controllers/UsersController.cs
@@ -1,17 +1,21 @@
 ﻿namespace API.Authentication.Controllers
 {
     using API.Authentication.DTO.Users;
+    using AutoMapper;
     using Business.Authentication.Attributes;
     using Business.Authentication.Interfaces;
     using Business.Authentication.Models;
     using Business.Authentication.Services;
     using Common.Encoding.Hash;
+    using Common.ExceptionHandler.Exceptions;
     using Common.Pagination;
     using Common.Pagination.Models;
+    using Data.Authentication.Globalization.Errors;
     using Data.Authentication.Models;
     using Microsoft.AspNetCore.Mvc;
     using Newtonsoft.Json;
     using System;
+    using System.Collections.Generic;
     using System.Linq;
     using System.Threading.Tasks;
 
@@ -28,13 +32,20 @@
         private readonly IUserService _userService;
 
         /// <summary>
+        /// Mapper
+        /// </summary>
+        private readonly IMapper _mapper;
+
+        /// <summary>
         /// Constructor
         /// </summary>
         /// <param name="userService"></param>
+        /// <param name="mapper"></param>
         /// <exception cref="ArgumentNullException"></exception>
-        public UsersController(IUserService userService)
+        public UsersController(IUserService userService, IMapper mapper)
         {
             _userService = userService ?? throw new ArgumentNullException(nameof(userService));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
         }
 
         /// <summary>
@@ -60,22 +71,13 @@
         public async Task<ActionResult> List([FromQuery] PaginationParams pagination)
         {
             //List users
-            var users = await _userService.ListAsync(pagination);
+            PagedList<User> users = await _userService.ListAsync(pagination);
 
             //Add pagination header on response
             Response.Headers.Add("x-pagination", JsonConvert.SerializeObject(users.GetMetaData()));
 
             //Build DTO objects
-            var userDTOs = users.Select(x => new UserDTO()
-            {
-                CreateDate = x.CreateDate,
-                Email = x.Email,
-                FirstName = x.FirstName,
-                Id = x.Id,
-                LastName = x.LastName,
-                Status = x.Status,
-                UpdateDate = x.UpdateDate
-            }).ToList();
+            List<UserDTO> userDTOs = _mapper.Map<List<UserDTO>>(users);
 
             return Ok(userDTOs);
         }
@@ -89,19 +91,10 @@
         public async Task<ActionResult> Get(int id)
         {
             //Load user
-            var user = await _userService.LoadAsync(id);
+            User user = await _userService.LoadAsync(id);
 
             //Build user DTO
-            var userDTO = new UserDTO()
-            {
-                CreateDate = user.CreateDate,
-                Email = user.Email,
-                FirstName = user.FirstName,
-                Id = user.Id,
-                LastName = user.LastName,
-                Status = user.Status,
-                UpdateDate = user.UpdateDate
-            };
+            UserDTO userDTO = _mapper.Map<UserDTO>(user);
 
             return Ok(userDTO);
         }
@@ -115,15 +108,7 @@
         public async Task<ActionResult> Post(CreateUserDTO createUser)
         {
             //Build user object
-            var user = new User()
-            {
-                CreateDate = DateTime.Now,
-                Email = createUser.Email,
-                FirstName = createUser.FirstName,
-                LastName = createUser.LastName,
-                Password = createUser.Password.ToSHA256(),
-                Status = 1
-            };
+            User user = _mapper.Map<User>(createUser);
 
             //Save user obj and return saved id
             return Ok(await _userService.SaveAsync(user));
@@ -138,12 +123,12 @@
         [HttpPut("{id}")]
         public async Task<ActionResult> Put(int id, UpdateUserDTO updateUser)
         {
-            var user = new User()
-            {
-                FirstName = updateUser.FirstName,
-                LastName = updateUser.LastName,
-                Status = updateUser.Status
-            };
+            User oldUser = await _userService.LoadAsync(id);
+
+            if (oldUser == null)
+                throw new NotFoundException(Errors.UserNotFound);
+
+            User user = _mapper.Map(updateUser, oldUser);
 
             //Save user obj and return saved id
             return Ok(await _userService.UpdateAsync(id, user));
---
+++ API.Authentication/API.Authentication/Mapper/AutoMapperBootstrapper.cs
@@ -0,0 +1,33 @@
+﻿using API.Authentication.DTO.Users;
+using AutoMapper;
+using Common.Encoding.Hash;
+using Data.Authentication.Models;
+using System;
+
+namespace API.Authentication.Mapper
+{
+    /// <summary>
+    /// Automapper bootstrapper
+    /// </summary>
+    public class AutoMapperBootstrapper : Profile 
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public AutoMapperBootstrapper()
+        {
+            CreateMap<User, UserDTO>();
+
+            CreateMap<CreateUserDTO, User>()
+                .ForMember(dest => dest.Password, opt => opt.MapFrom(src => src.Password.ToSHA256()))
+                .ForMember(dest => dest.Status, opt => opt.MapFrom(src => 1))
+                .ForMember(dest => dest.CreateDate, opt => opt.MapFrom(src => DateTime.Now));
+
+            CreateMap<UpdateUserDTO, User>()
+                .ForMember(dest => dest.UpdateDate, opt => opt.MapFrom(src => DateTime.Now))
+                .ForMember(x => x.Email, opt => opt.Ignore())
+                .ForMember(x => x.Id, opt => opt.Ignore())
+                .ForMember(x => x.CreateDate, opt => opt.Ignore());
+        }
+    }
+}
